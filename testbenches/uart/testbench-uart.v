307cb512bedc4643ea59339439df525b3d3c112f
Matthias Bock <mail@matthiasbock.net>
2018-09-26 13:59:10 +0200

    Implemented UART receiver and a testbench for it

---
+++ testbenches/uart/testbench-uart.v
@@ -0,0 +1,91 @@
+`timescale 1ns/1ns
+
+`include "../../uart.v"
+
+module test;
+
+initial $dumpvars;
+
+
+reg reset;
+initial #1 reset <= 1;
+initial #8 reset <= 0;
+
+reg clock_144mhz;
+initial clock_144mhz <= 0;
+always #1 clock_144mhz <= ~clock_144mhz;
+
+reg clock_12mhz;
+initial clock_12mhz <= 0;
+reg[2:0] clock_12mhz_counter = 0;
+always @(posedge clock_144mhz)
+begin
+    if (clock_12mhz_counter == 5)
+    begin
+        clock_12mhz_counter <= 0;
+        clock_12mhz <= ~clock_12mhz;
+    end
+    else begin
+        clock_12mhz_counter <= clock_12mhz_counter + 1;
+    end
+end
+
+reg clock_115200hz;
+initial clock_115200hz <= 0;
+reg[9:0] clock_115200hz_counter = 0;
+always @(posedge clock_144mhz)
+begin
+    if (clock_115200hz_counter == 625)
+    begin
+        clock_115200hz_counter <= 0;
+        clock_115200hz <= ~clock_115200hz;
+    end
+    else begin
+        clock_115200hz_counter <= clock_115200hz_counter + 1;
+    end
+end
+
+/*
+ * Test signal generation
+ */
+reg uart_rx;
+reg uart_rts;
+wire uart_cts;
+wire[7:0] data;
+wire data_ready;
+
+reg[7:0] test_data = 8'b01011001;
+initial
+begin
+    uart_rx <= 0;
+    // Start
+    #900 uart_rx <= 1;
+    // 8 bits of data, in LSB-first order
+    #2500 uart_rx <= ~test_data[0];
+    #2500 uart_rx <= ~test_data[1];
+    #2500 uart_rx <= ~test_data[2];
+    #2500 uart_rx <= ~test_data[3];
+    #2500 uart_rx <= ~test_data[4];
+    #2500 uart_rx <= ~test_data[5];
+    #2500 uart_rx <= ~test_data[6];
+    #2500 uart_rx <= ~test_data[7];
+    // Stop bit
+    #2500 uart_rx <= 0;
+end
+
+// End of test signal generation
+initial #25000 $finish;
+
+
+/** Module under test */
+uart uart0(
+    .clock_115200hz(clock_115200hz),
+    .reset(reset),
+    .rx(uart_rx),
+    .rts(uart_rts),
+    .cts(uart_cts),
+    .rx_data(data),
+    .rx_data_ready(data_ready)
+    );
+
+endmodule
---
+++ uart.v
@@ -0,0 +1,77 @@
+
+`ifndef UART_V
+`define UART_V
+
+module uart(
+    input clock_115200hz,
+    input reset,
+
+    input rx,
+    input rts,
+    output cts,
+
+    output reg receiving,
+    output reg[7:0] rx_data,
+    output reg rx_data_ready
+    );
+
+/** Grant all transmission requests */
+assign cts = rts;
+
+/*
+ * Receive bits
+ */
+reg[3:0] bit_counter;
+reg[2:0] pause_counter;
+wire pause_is_over = pause_counter[2];
+
+always @(posedge clock_115200hz or posedge reset)
+begin
+    if (reset)
+    begin
+        bit_counter <= 0;
+        pause_counter <= 3'b100;
+        rx_data <= 0;
+        rx_data_ready <= 0;
+        receiving <= 0;
+    end
+    else begin
+        if (receiving)
+        begin
+            // Sample and store the value at the RX input pin
+            rx_data[bit_counter] <= ~rx;
+
+            if (bit_counter == 7)
+            begin
+                // Reception of 8 bit is complete
+                receiving <= 0;
+                rx_data_ready <= 1;
+            end
+            else begin
+                // Decrement bit counter
+                bit_counter <= bit_counter + 1;
+            end
+        end
+        else begin
+            // Clear the previously set ready flag
+            rx_data_ready <= 0;
+            bit_counter <= 0;
+
+            // After receiving wait at least 5 bits before re-enabling the receiver
+            if (~pause_is_over)
+            begin
+                pause_counter <= pause_counter + 1;
+            end
+
+            if (pause_is_over && rx)
+            begin
+                // After a short pause a start bit was received
+                receiving <= 1;
+            end
+        end
+    end
+end
+
+endmodule
+
+`endif

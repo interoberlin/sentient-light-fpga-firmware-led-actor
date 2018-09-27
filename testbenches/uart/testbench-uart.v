bf67a28d13ad0c84d335eb779d75ee3aef9cf473
Matthias Bock <mail@matthiasbock.net>
2018-09-27 11:41:24 +0200

    testbench-uart.v: Adjusted testbench to match the actual voltage levels on the FPGA board

--- testbenches/uart/testbench-uart.v
+++ testbenches/uart/testbench-uart.v
@@ -54,27 +54,27 @@ wire uart_cts;
 wire[7:0] data;
 wire data_ready;
 
-reg[7:0] test_data = 8'b01011001;
+reg[7:0] test_data = 32'h62; //8'b01011001;
 initial
 begin
-    uart_rx <= 0;
+    uart_rx <= 1;
     // Start
-    #900 uart_rx <= 1;
+    #6000 uart_rx <= 0;
     // 8 bits of data, in LSB-first order
-    #2500 uart_rx <= ~test_data[0];
-    #2500 uart_rx <= ~test_data[1];
-    #2500 uart_rx <= ~test_data[2];
-    #2500 uart_rx <= ~test_data[3];
-    #2500 uart_rx <= ~test_data[4];
-    #2500 uart_rx <= ~test_data[5];
-    #2500 uart_rx <= ~test_data[6];
-    #2500 uart_rx <= ~test_data[7];
+    #2500 uart_rx <= test_data[0];
+    #2500 uart_rx <= test_data[1];
+    #2500 uart_rx <= test_data[2];
+    #2500 uart_rx <= test_data[3];
+    #2500 uart_rx <= test_data[4];
+    #2500 uart_rx <= test_data[5];
+    #2500 uart_rx <= test_data[6];
+    #2500 uart_rx <= test_data[7];
     // Stop bit
-    #2500 uart_rx <= 0;
+    #2500 uart_rx <= 1;
 end
 
 // End of test signal generation
-initial #25000 $finish;
+initial #40000 $finish;
 
 
 /** Module under test */

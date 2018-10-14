04b7f6c2e0ce679170e91416a2a086537fed4905
Matthias Bock <mail@matthiasbock.net>
2018-10-14 18:29:53 +0200

    testbenches/clock_generator/: Brought tests up to speed

--- testbenches/clock_generator/testbench-clocks.v
+++ testbenches/clock_generator/testbench-clocks.v
@@ -6,25 +6,37 @@ module test;
 
 initial $dumpvars;
 
-reg clock_12mhz, reset;
+reg clock_144mhz;
+reg clock_12mhz;
+reg reset;
 
+initial clock_144mhz <= 0;
 initial clock_12mhz <= 0;
-always #1 clock_12mhz <= ~clock_12mhz;
-
+always #1 clock_144mhz <= ~clock_144mhz;
+always #12 clock_12mhz <= ~clock_12mhz;
 
 // End of test signal generation
-initial #2000 $finish;
+initial #24000 $finish;
 
+/*
+ * Test the synchronization of the 115200 Hz clock
+ * to the UART RX signal
+ */
+reg uart_rx;
+initial uart_rx <= 0;
+initial #500 uart_rx <= 1;
 
 /** Module under test */
 wire bit_segment_clock, bit_clock, led_clock, framerate;
 
 clock_generator clocks(
+    .clock_144mhz(clock_144mhz),
     .clock_12mhz(clock_12mhz),
     .bit_segment_clock(bit_segment_clock),
     .bit_clock(bit_clock),
     .led_clock(led_clock),
-    .framerate(framerate)
+    .framerate(framerate),
+    .uart_rx(uart_rx)
     );
 
 endmodule
